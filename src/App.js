e57f030ebc5c0e1be2ada006b1da95410a8cae44
Kareem <kareemj.williams@gmail.com>
2021-03-03 13:17:31 -0500

    componentization and prop drilling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
-import Coin from './components/Coin/Coin';
+import CoinList from './components/CoinList/CoinList';
 import AccountBalance from './components/AccountBalance/AccountBalance'
+import Header from './components/Header/Header'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="React logo" className = "App-logo" />
-        <h1 className ="App-title">
-          Coin Exchange 
-        </h1>
-      </header>
-      <AccountBalance amount = {10000} />
-      <table className= "Coin-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Coin name="Bitcoin" ticker= "BTC" price = {9999.99} /> 
-          <Coin name="Ethereum" ticker= "ETH" price = {299.99} /> 
-          <Coin name="Tether" ticker="USDT" price={1.0}/>
-          <Coin name="Ripple" ticker="XRP" price={0.2}/>
-        </tbody>
-      </table>
-    </div>
-  );
+const Div = styled.div `
+  text-align: center;
+  background-color: rgb(55, 119, 119);
+  color: #cccccc;
+`
+
+//import { v4 as uuidv4} from 'uuid'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+          {
+            name: 'Bitcoin',
+            ticker: 'BTC',
+            price: 9999.99
+          },
+          {
+            name: 'Ethereum',
+            ticker: 'ETH',
+            price: 299.99
+          },
+          {
+            name: 'Tether',
+            ticker: 'USDT',
+            price: 1.0
+          },
+          {
+            name: 'Ripple',
+            ticker:'XRP',
+            price: 0.2
+          },
+          {
+            name: 'Bitcoin Cash',
+            ticker:'BCH',
+            price: 298.99
+          }
+
+            // <Coin name="Bitcoin" ticker= "BTC" price = {9999.99} /> 
+            // <Coin name="Ethereum" ticker= "ETH" price = {299.99} /> 
+            // <Coin name="Tether" ticker="USDT" price={1.0}/>
+            // <Coin name="Ripple" ticker="XRP" price={0.2}/>
+
+      ]
+    }
+  }
+  render(){
+    return (
+      <Div>
+        <Header />
+        <AccountBalance amount = {this.state.balance} />
+        <CoinList coinData = {this.state.coinData} />
+      </Div>
+    );
+  }
 }
 
 export default App;
